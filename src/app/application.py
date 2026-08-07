6920b43edc851239ee3804e22fb08beb3867ea4f
Cícero Jorge Leandro de Sousa <cicerosousale@gmail.com>
2026-08-07 12:38:38 -0300

    feat: adiciona módulo inicial de descoberta da rede

--- src/app/application.py
+++ src/app/application.py
@@ -3,11 +3,19 @@ Inicialização da aplicação.
 """
 
 from app.menu import Menu
+from scanner.network import NetworkService
 
 
 class Application:
 
     def run(self) -> None:
+
+        network = NetworkService.get_network_info()
+
+        print("\nInformações da máquina:")
+        print(f"Hostname: {network.hostname}")
+        print(f"IP: {network.ip_address}")
+        print()
 
         while True:
 
